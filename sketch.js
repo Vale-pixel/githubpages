ea3e319234307b947b0ce10803e2e41442a8e9fb
Vale-pixel <100896964@u.icesi.edu.co>
2021-05-24 22:13:27 -0500

    upload files

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  fill(0);
+  textSize(20);
+  text('Valeria',50,50,100,200);
+
+  fill(0);
+  ellipse
+}
